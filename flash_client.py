ddc42d6cc8b0973400c27afe88ea48b41cbc3804
Cargill Seiveright <microcargill@hotmail.com>
2020-06-17 04:14:18 -0500

    Automatically create gnupg folder for client

--- flash_client.py
+++ flash_client.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 
 from keyM.pgpier import *
-import requests, hashlib
+import requests, hashlib, os
 
 CLIENT_NAME = 'Pgpier Client'
 CLIENT_EMAIL = 'client_pgpier@gmail.com'
 CLIENT_COMMENT = 'Pgpier Client created for encrypted communication'
 
-gpg = Pgpier('/home/cargill/Documents/GnuPG-demo/keys/.gnupg')
-gpg.set_from_imp()
-gpg.set_keyid()
+result = create_dir('cligpg')
+if result[0] == True:
+    cli_dir = result[1]
+    gnupghome = create_dir('.gnupg', True, cli_dir)
 
-pubkey = gpg.exp_pub_key()
-b_hash = pubkey
+    if gnupghome[0] == True:
+        gnupg_dir = gnupghome[1]
+        print(gnupg_dir)
+        gpg = Pgpier(gnupg_dir)
+    else:
+        raise Exception('Could not create gnupg folder to store key')
+else:
+    raise Exception('Could not create client folder')
+
+
+#gpg = Pgpier('/home/cargill/Documents/GnuPG-demo/keys/.gnupg')
+#gpg.set_from_imp()
+#gpg.set_keyid()
+
+pubkey = "public key" #gpg.exp_pub_key()
+b_hash = "public key" #pubkey
 
 pubhash = hashlib.sha256(b_hash.encode('utf-8')).hexdigest()
 
